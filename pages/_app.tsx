8b26680aa72a8bbcf15d7ffe4381ebd3f32b3f81
Tony <diyorbeksolijonov27@gmail.com>
2025-10-14 17:30:05 +0900

    feat: integrating mui

--- pages/_app.tsx
+++ pages/_app.tsx
@@ -1,6 +1,17 @@
-import "@/styles/globals.css";
 import type { AppProps } from "next/app";
+import {ThemeProvider, createTheme} from "@mui/material/styles"
+import { CssBaseline } from "@mui/material";
+import {light} from "../scss/MaterialTheme/index"
 
 export default function App({ Component, pageProps }: AppProps) {
-  return <Component {...pageProps} />;
+  //@ts-ignore
+  const [theme,setTheme] = useState(createTheme(light))
+  /**socket.io, Redux , mui*/
+
+  return
+  <ThemeProvider theme={theme}>
+    <CssBaseline/>
+    <Component {...pageProps} />;
+  </ThemeProvider>
+
 }
